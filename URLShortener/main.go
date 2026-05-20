e581016abaaef85ce87a3353d47ffb72cda490a9
EntropicProblems <155194337+EntropicProblems@users.noreply.github.com>
2026-05-20 05:23:03 -0400

    Adding redirecting to the webpage, dont know if it works yet

--- URLShortener/main.go
+++ URLShortener/main.go
@@ -27,6 +27,14 @@ func loadPage(title string) (*Page, error) {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+    //need to take a request for a specific path in our domain 
+    //and check and see if it maps to any url's
+    if r.URL.Path == "/" {
+
+    } else {
+        //http.Redirect(w,r,,301) //half-built redirect function
+    }
+    //if the domain is root, direct them to a page to designate a new url to shorten
     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
