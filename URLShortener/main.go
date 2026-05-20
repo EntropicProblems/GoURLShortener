5bb1aebbffd83ac53e7015b3130d7ce111370509
EntropicProblems <155194337+EntropicProblems@users.noreply.github.com>
2026-05-20 11:44:07 -0400

    Got the json working supposedly, now to implement the http POST in the html

--- URLShortener/main.go
+++ URLShortener/main.go
@@ -27,6 +27,8 @@ func loadPage(title string) (*Page, error) {
     return &Page{Title: title, Body: body}, nil
 }
 
+var myshortcuts = make(map[string]string)//fix this and then we can get cooking with POST
+
 func handler(w http.ResponseWriter, r *http.Request) {
     //need to take a request for a specific path in our domain 
     //and check and see if it maps to any url's
@@ -34,7 +36,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
         if r.URL.Path == "/" || r.URL.Path == "/login" || r.URL.Path =="/register" {
             //do nothing
         } else {
-            http.Redirect(w,r,"https://google.com",301) //half-built redirect function
+            if myshortcuts == nil {
+                body, err := os.ReadFile("shortcuts.json")
+                if err != nil {
+                    fmt.Fprint(w, "Something went wrong") //adjust this to a nice webpage eventually
+                }
+                err = json.Unmarshal(body, &myshortcuts)
+                if err != nil {
+                    fmt.Fprint(w, "Something went wrong") //adjust this to a nice webpage eventually
+                }
+            }
+            http.Redirect(w,r,myshortcuts[r.URL.Path],301) //half-built redirect function
         }
         //if the domain is root, direct them to a page to designate a new url to shorten
         fmt.Fprint(w, "Welcome to root")
@@ -46,11 +58,34 @@ func handler(w http.ResponseWriter, r *http.Request) {
             //these are invalid inputs, so throw them back an error in the form
         } else {
             //add to the map
+            if myshortcuts == nil {
+                body, err := os.ReadFile("shortcuts.json")
+                if err != nil {
+                    fmt.Fprint(w, "Something went wrong") //adjust this to a nice webpage eventually
+                }
+                err = json.Unmarshal(body, &myshortcuts)
+                if err != nil {
+                    fmt.Fprint(w, "Something went wrong") //adjust this to a nice webpage eventually
+                }
+            }
+            _, exists := myshortcuts[r.FormValue("URLShortcut")]
+            if exists {
+                fmt.Fprint(w, "Whoops! Someone already is using this shortcut!")
+            } else {
+                myshortcuts[r.FormValue("URLShortcut")] = r.FormValue("ShortcuttedURL")
+                //update the json now
+                byties, err :=json.Marshal(myshortcuts)
+                if err != nil {
+                    fmt.Fprint(w, "Something went wrong") //adjust this to a nice webpage eventually
+                }
+                os.WriteFile("shortcuts.json", byties, 0600)
+            }
         }
     }
 }
 
 func main(){
+    myshortcuts = nil
     http.HandleFunc("/", handler)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
